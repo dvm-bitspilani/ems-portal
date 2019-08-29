9836168cd926763e88a69e587d7ed6f108bbd182
Shreyans Jain <shreyansj551@gmail.com>
2019-08-29 14:38:23 +0530

    add headers

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,12 +1,4 @@
-import React from "react";
-// import { Link } from "react-router-dom";
-
-// import Avatar from "@material-ui/core/Avatar";
-// import Button from "@material-ui/core/Button";
-// import CssBaseline from "@material-ui/core/CssBaseline";
-// import TextField from "@material-ui/core/TextField";
-// import Typography from "@material-ui/core/Typography";
-// import Container from "@material-ui/core/Container";
+import React, { Fragment } from "react";
 
 import {
   Avatar,
@@ -18,6 +10,10 @@ import {
 } from "@material-ui/core";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import { makeStyles } from "@material-ui/core/styles";
+
+// --------------------------------------------------------------------
+// Uses standard material ui styles to generate a Google-style login page
+// --------------------------------------------------------------------
 
 const useStyles = makeStyles(theme => ({
   "@global": {
@@ -31,12 +27,18 @@ const useStyles = makeStyles(theme => ({
     flexDirection: "column",
     alignItems: "center"
   },
+  header: {
+    marginTop: theme.spacing(5)
+  },
+  caption: {
+    margin: theme.spacing(3)
+  },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main
   },
   form: {
-    width: "100%", // Fix IE 11 issue.
+    width: "100%", 
     marginTop: theme.spacing(1)
   },
   submit: {
@@ -48,59 +50,62 @@ export default function SignIn() {
   const classes = useStyles();
 
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign in
+    <Fragment>
+      <Typography
+        component="h1"
+        variant="h3"
+        align="center"
+        className={classes.header}
+      >
+        OASIS'19 EMS
+      </Typography>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Typography variant="h5" align="center" className={classes.caption}>
+          Event Management System
         </Typography>
-        <form className={classes.form} noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign In
-          </Button>
-          {/* <Button
-            fullWidth
-            variant="contained"
-            color="secondary"
-            className={classes.submit}
-            component={Link}
-            to="/signup"
-          >
-            Switch to Sign up
-          </Button> */}
-        </form>
-      </div>
-    </Container>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <form className={classes.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign In
+            </Button>
+          </form>
+        </div>
+      </Container>
+    </Fragment>
   );
 }
