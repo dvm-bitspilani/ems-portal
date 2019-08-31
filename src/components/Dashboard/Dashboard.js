9f58f5304ac351558cdd64c4475b30dd520d1030
Shreyans Jain <shreyansj551@gmail.com>
2019-08-31 20:19:19 +0530

    add events panel, levels list

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,10 +1,27 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Paper, Typography } from "@material-ui/core";
+import { Paper, Typography, Divider } from "@material-ui/core";
+import { grey } from "@material-ui/core/colors";
+
+import Events from "./Events"
 
 const useStyles = makeStyles(theme => ({
   root: {
-    padding: theme.spacing(3,2)
+    padding: theme.spacing(3, 2)
+  },
+  boldText: {
+    fontWeight: "bold"
+  },
+  dashboard: {
+    color: grey[600],
+    margin: theme.spacing(2,0,2,2),
+    fontWeight: "bold"
+  },
+  greet: {
+    fontWeight: "bold"
+  },
+  caption: {
+    margin: theme.spacing(1,0)
   }
 }));
 
@@ -12,11 +29,13 @@ const Dashboard = () => {
   const classes = useStyles();
   return (
     <div>
+      <Typography component="p" className={classes.dashboard}>DASHBOARD</Typography>
       <Paper className={classes.root}>
-        <Typography variant="h5">Dashboard</Typography>
-        <Typography componentf="p">
-          Hello raans what are you doing. Chalo saath me dota khelte hain. Oh sorry dota to outdated hai
-        </Typography>
+        <Typography variant="h4">HELLO JUDGES!</Typography>
+        <Typography component="p" className={classes.caption}>Please select an event:</Typography>
+        <Divider style={{margin: "20px 0"}}/>
+
+        <Events />
       </Paper>
     </div>
   );
