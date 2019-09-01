8a7f3591e2c7880e5ed1404b33f6b9734b314909
Akshit-Patel <akshitpatel72@gmail.com>
2019-09-01 20:20:40 +0530

    merge conflicts

--- src/containers/Level/level.js
+++ src/containers/Level/level.js
@@ -1,15 +1,10 @@
 import React from "react";
-<<<<<<< HEAD
 import Level from "../../components/Events/Level";
 
 class LevelContainer extends React.Component{
     constructor(props) {
         super(props);
     }
-=======
-// import { ReactComponent } from "*.svg";
-class Level extends React.Component{
->>>>>>> f5a7d49ab2faa1f26ff69f9274364f52725adb6f
     render(){
         return(
             <Level/>
