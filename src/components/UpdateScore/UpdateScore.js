55dbdfc7438702042d142e9feb35a1e76521567f
Shreyans Jain <shreyansj551@gmail.com>
2019-10-06 18:33:40 +0530

    add view for score update

---
+++ src/components/UpdateScore/UpdateScore.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Typography} from "@material-ui/core"
+const UpdateScore = () => {
+  return (
+    <div>
+      <Typography>Update Score</Typography>
+    </div>
+  )
+}
+
+export default UpdateScore
