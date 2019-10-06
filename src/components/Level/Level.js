892725688094986cf3ded62da963cc756e280e4c
Shreyans Jain <shreyansj551@gmail.com>
2019-10-06 10:15:37 +0530

    render teams from response

--- src/components/Level/Level.js
+++ src/components/Level/Level.js
@@ -43,32 +43,38 @@ class Level extends React.Component {
 
   render() {
     console.log(this.props)
+    const teams = [...this.state.teams]
+
+    // structure of teams array from request
+    // teams: [
+    //   {
+    //     teamName,
+    //     teamId,
+    //     score
+    //   } 
+    //    ...
+    //    ...
+    // ]
     return (
       <Container fixed>
         <h1>Teams</h1>
         <div fixed="true" className="container">
-          <div className="teamName">
-            <ListItemText primary={"Team Name"} className="link" />
-            <ListItemText primary={"Total Score: 5"} className="link" />
-            <Button variant="contained" color="primary" className="button">
-              Update Score
-            </Button>
-            <Button variant="contained" color="secondary" className="button">
-              Freeze
-            </Button>
-          </div>
-          <div className="teamName">
-            {/* <ListItem button> */}
-            <ListItemText primary={"Team Name"} className="link" />
-            <ListItemText primary={"Total Score: 5"} className="link" />
-            <Button variant="contained" color="primary" className="button">
-              Update Score
-            </Button>
-            <Button variant="contained" color="secondary" className="button">
-              Freeze
-            </Button>
-            {/* </ListItem> */}
-          </div>
+          {teams.map((team, index) => {
+            const {teamName, score} = team;
+
+            return (
+              <div className="teamName" key={index}>
+                <ListItemText primary={`${teamName}`} className="link" />
+                <ListItemText primary={`Total Score: ${score}`} className="link" />
+                <Button variant="contained" color="primary" className="button">
+                  Update Score
+                </Button>
+                <Button variant="contained" color="secondary" className="button">
+                  Freeze
+                </Button>
+              </div>
+            )
+          })}
         </div>
       </Container>
     );
