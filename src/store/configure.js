56ffa42a4c3425343ff52f4e9c281f74d455f3f6
Shreyans Jain <shreyansj551@gmail.com>
2019-10-09 15:18:28 +0530

    connect to score reducer to store

--- src/store/configure.js
+++ src/store/configure.js
@@ -1,15 +1,16 @@
 import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import thunk from "redux-thunk";
-// import levelReducer from "../store/reducers/level";
 import eventsReducer from "../store/reducers/events";
 import teamsReducer from "../store/reducers/teams";
+import scoreReducer from "../store/reducers/score";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const rootReducer = combineReducers({
   // level: levelReducer,
   events: eventsReducer,
-  teams: teamsReducer
+  teams: teamsReducer,
+  score: scoreReducer
 });
 
 const configureStore = () => {
